3eee757b6af6dfb1ebfc5cf4c459f2b0ebba27bb
zhiqiang <zhang@zhiqiang.org>
2019-02-19 21:50:37 +0800

    add similar score

--- similar_posts.py
+++ similar_posts.py
@@ -62,6 +62,8 @@ def add_similar_posts(generator):
             reverse=True)[:max_count]
 
         article.similar_posts = [generator.articles[idx] for idx, _ in selected]
+        article.similar_scores = [score for _, score in selected]
+        article.similar = [(generator.articles[idx], score) for idx, score in selected]
 
         logger.debug('{article}: similar_posts scores: {scores}'.format(
             article=os.path.basename(article.source_path) if hasattr(article, 'source_path') else i,
